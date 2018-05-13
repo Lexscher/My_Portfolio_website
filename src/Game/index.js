ecfd523f3c442731ec57b03e41c73d1f78416f41
Alexander Schelchere <ascellie@gmail.com>
2018-05-13 09:58:14 -0400

    Whoops! Had to install react-particles-js -- fixed it!

--- src/Game/index.js
+++ src/Game/index.js
@@ -12,7 +12,9 @@ class Game extends Component {
         <header>
           <h1>Game Title</h1>
         </header>
-        <div className="game-play" />
+        <div className="game-play">
+          <div id="particles-js" />
+        </div>
         <footer>
           <h2>Have Fun!</h2>
         </footer>
