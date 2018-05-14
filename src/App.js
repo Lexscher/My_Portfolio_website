83f40380ae7ac4179a033619851da2fd787bcf8b
Alexander Schelchere <ascellie@gmail.com>
2018-05-13 22:37:10 -0400

    The game component isn't _really_ a game anymore😅 But have fun with the particlesgit add .

--- src/App.js
+++ src/App.js
@@ -12,144 +12,31 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Particles id="particles-js" params={particleParams}>
-          <div className="container">
-            <nav className="nav-bar">
-              <Link className="links" to="/">
-                Home
-              </Link>
-              <Link className="links" to="/alexander">
-                About Me
-              </Link>
-              <Link className="links" to="/projects">
-                Projects I've worked on
-              </Link>
-              <Link className="links" to="/play">
-                Check out this fun game!
-              </Link>
-            </nav>
+        <div className="container">
+          <nav className="nav-bar">
+            <Link className="links" to="/">
+              Home
+            </Link>
+            <Link className="links" to="/alexander">
+              About Me
+            </Link>
+            <Link className="links" to="/projects">
+              Projects I've worked on
+            </Link>
+            <Link className="links" to="/particles">
+              Check this out!
+            </Link>
+          </nav>
+          <div className="content">
             <Route exact path="/" render={() => <Home />} />
             <Route exact path="/alexander" render={() => <AboutMe />} />
             <Route exact path="/projects" render={() => <Portfolio />} />
-            <Route exact path="/play" render={() => <Game />} />
+            <Route exact path="/particles" render={() => <Game />} />
           </div>
-        </Particles>
+        </div>
       </Router>
     );
   }
 }
-
-// kinda remembered the stylesheet and stackNavigator in React Native.
-// Making a constant to hold the parameters, and then passing it to the Particles seems a lot cleaner.
-const particleParams = {
-  particles: {
-    number: {
-      value: 40,
-      density: {
-        enable: true,
-        value_area: 800
-      }
-    },
-    color: {
-      value: "#00D1FF"
-    },
-    shape: {
-      type: "circle",
-      stroke: {
-        width: 2,
-        color: "#00D1FF"
-      },
-      polygon: {
-        nb_sides: 5
-      },
-      image: {
-        src: "https://i.imgur.com/RYVm3qh.jpg",
-        width: 100,
-        height: 100
-      }
-    },
-    opacity: {
-      value: 0.1,
-      random: false,
-      anim: {
-        enable: false,
-        speed: 1,
-        opacity_min: 0.1,
-        sync: false
-      }
-    },
-    size: {
-      value: 7,
-      random: true,
-      anim: {
-        enable: false,
-        speed: 10,
-        size_min: 0.1,
-        sync: false
-      }
-    },
-    line_linked: {
-      enable: true,
-      distance: 125,
-      color: "#EE1616",
-      opacity: 0.7,
-      width: 1.5
-    },
-    move: {
-      enable: true,
-      speed: 3,
-      direction: "none",
-      random: false,
-      straight: false,
-      out_mode: "out",
-      bounce: false,
-      attract: {
-        enable: false,
-        rotateX: 600,
-        rotateY: 1200
-      }
-    }
-  },
-  interactivity: {
-    detect_on: "canvas",
-    events: {
-      onhover: {
-        enable: true,
-        mode: ["bubble", "repulse"]
-      },
-      onclick: {
-        enable: true,
-        mode: "push"
-      },
-      resize: true
-    },
-    modes: {
-      grab: {
-        distance: 800,
-        line_linked: {
-          opacity: 1
-        }
-      },
-      bubble: {
-        distance: 200,
-        size: 4,
-        duration: 2,
-        opacity: 1,
-        speed: 3
-      },
-      repulse: {
-        distance: 75,
-        duration: 0.4
-      },
-      push: {
-        particles_nb: 5
-      },
-      remove: {
-        particles_nb: 2
-      }
-    }
-  },
-  retina_detect: true
-};
 
 export default App;
--- src/Game/index.js
+++ src/Game/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import Particles from "react-particles-js";
 class Game extends Component {
   constructor(props) {
     super(props);
@@ -9,18 +9,125 @@ class Game extends Component {
   render() {
     return (
       <div className="game-container">
-        <header>
-          <h1>Game Title</h1>
-        </header>
-        <div className="game-play">
-          <div id="particles-js" />
-        </div>
-        <footer>
-          <h2>Have Fun!</h2>
-        </footer>
+        <Particles id="particles-js" params={particleParams}>
+          <h1 className="h">hello</h1>
+        </Particles>
       </div>
     );
   }
 }
+
+// kinda remembered the stylesheet and stackNavigator in React Native.
+// Making a constant to hold the parameters, and then passing it to the Particles seems a lot cleaner.
+const particleParams = {
+  particles: {
+    number: {
+      value: 40,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: "#00D1FF"
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 2,
+        color: "#00D1FF"
+      },
+      polygon: {
+        nb_sides: 5
+      },
+      image: {
+        src: "https://i.imgur.com/RYVm3qh.jpg",
+        width: 100,
+        height: 100
+      }
+    },
+    opacity: {
+      value: 0.1,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false
+      }
+    },
+    size: {
+      value: 8,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 10,
+        size_min: 0.1,
+        sync: false
+      }
+    },
+    line_linked: {
+      enable: true,
+      distance: 125,
+      color: "#EE1616",
+      opacity: 0.7,
+      width: 1.5
+    },
+    move: {
+      enable: true,
+      speed: 3,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200
+      }
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: ["bubble", "repulse", "grab"]
+      },
+      onclick: {
+        enable: true,
+        mode: "push"
+      },
+      resize: true
+    },
+    modes: {
+      grab: {
+        distance: 125,
+        line_linked: {
+          opacity: 0.2
+        }
+      },
+      bubble: {
+        distance: 200,
+        size: 4,
+        duration: 2,
+        opacity: 1,
+        speed: 3
+      },
+      repulse: {
+        distance: 75,
+        duration: 0.4
+      },
+      push: {
+        particles_nb: 5
+      },
+      remove: {
+        particles_nb: 2
+      }
+    }
+  },
+  retina_detect: true
+};
 
 export default Game;
